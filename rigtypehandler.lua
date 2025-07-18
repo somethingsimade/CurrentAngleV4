de4f9eade3c21065c8b215637a8d3275ed7f9b8e
MrY7zz <201977713+somethingsimade@users.noreply.github.com>
2025-07-18 00:52:22 -0300

    Update rigtypehandler.lua

--- rigtypehandler.lua
+++ rigtypehandler.lua
@@ -7,6 +7,9 @@
   Removing or modifying this notice may violate copyright law.
 ]]
 --// BY MrY7zz
+local R6 = Enum.HumanoidRigType.R6
+local R15 = Enum.HumanoidRigType.R15
+
 local LocalPlayer = game:GetService("Players").LocalPlayer
 
 if not LocalPlayer then
@@ -17,4 +20,8 @@ if not LocalPlayer.Character then
 	LocalPlayer.CharacterAdded:Wait()
 end
 task.wait(0.1)
-return LocalPlayer.Character:FindFirstChildOfClass("Humanoid").RigType
+if LocalPlayer.Character:FindFirstChildOfClass("Humanoid").RigType == R15 then
+	return R15
+else
+	return R6 --// This check could be better... but what else would the rig type be?
+end
