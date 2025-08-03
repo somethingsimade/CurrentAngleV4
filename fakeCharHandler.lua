2b7924e0fa963bbe6caca01c79b2a7a0efa6753d
MrY7zz <99chinesesteve@gmail.com>
2025-08-03 19:36:16 -0300

    Create fakeCharHandler.lua

---
+++ fakeCharHandler.lua
@@ -0,0 +1,31 @@
+
+--[[
+  Licensed under the MIT License (see LICENSE file for full details).
+  Copyright (c) 2025 MrY7zz
+
+  LEGAL NOTICE:
+  You are REQUIRED to retain this license header under the terms of the MIT License.
+  Removing or modifying this notice may violate copyright law.
+]]
+--// BY MrY7zz
+
+return (function(...)
+	local args = { ... }
+	local fakeCharacter = nil
+	local realChar = args[1] or game:GetService("Players").LocalPlayer.Character or game:GetService("Players").LocalPlayer.CharacterAdded:Wait()
+	local willClone = args[2]
+	local rigType = args[3]
+	
+	local Players = game:GetService("Players")
+	
+	if willClone then
+		local value = realChar.Archivable
+		realChar.Archivable = false
+		fakeCharacter = realChar:Clone()
+		realChar.Archivable = value
+		return fakeCharacter
+	else
+		fakeCharacter = Players:CreateHumanoidModelFromDescription(realChar.Humanoid:FindFirstChild("HumanoidDescription") or Instance.new("HumanoidDescription"), rigType)
+		return fakeCharacter
+	end
+end)(...)
