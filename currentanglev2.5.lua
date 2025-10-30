ae11320a297122200525db3a0f73415684a1e83c
MrY7zz <201977713+somethingsimade@users.noreply.github.com>
2025-10-29 21:13:42 -0300

    Update currentanglev2.5.lua

--- currentanglev2.5.lua
+++ currentanglev2.5.lua
@@ -537,6 +537,9 @@ local function flinginternal(character, time)
 		if character then
 			if character:FindFirstChild("HumanoidRootPart") then
 				local velocity = character.HumanoidRootPart.Velocity
+				if velocity.Unit.X + velocity.Unit.Y + velocity.Unit.Z >= 5000 then
+					return
+				end
 				local direction = velocity.Magnitude > 1 and velocity.Unit or Vector3_new(0, 0, 0)
 				local predictedPosition = (character.PrimaryPart.CFrame or character.HumanoidRootPart.CFrame).Position + direction * math_random(5, 12)
 				
